8f5380068cb71ea13bda92778483d5fa217cdbf8
CovaDax <covadax.ag@gmail.com>
2016-02-04 13:03:00 -0500

    Beginning to transfer code to new Library Project

---
+++ FFRKRLib/Interfaces/IModelRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFRKRLib.Interfaces {
+    public interface IModelRepository {
+        IEnumerable<T> GetAll<T>();
+        IEnumerable<T> GetAllByColumn<T>(string column);
+        T FindByName<T>(string name);
+        T FindColumnByName<T>(string name, string column);
+    }
+}
---
+++ FFRKRLib/Models/Ability.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFRKRLib.Models {
+    public class Ability {
+
+        #region Private Members
+        private string _name;
+        private int _rank; //0 means not crafted. 5 means complete.
+        private int _level;
+        private string _type;
+        private string _school;
+        private DataTable _cost;
+        private bool _maxed;
+        private bool _crafted;
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int Rank
+        {
+            get { return _rank; }
+        }
+
+        public int Level
+        {
+            get { return _level; }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+        }
+
+        public string School
+        {
+            get { return _school; }
+        }
+
+        public DataTable Cost
+        {
+            get { return _cost; }
+        }
+        #endregion
+
+    }
+}
